00d37b1293d6fe45df9e2798bd1b86e5fcfdb85e
Иванов Максим Андреевич <149088259+Sephirott1@users.noreply.github.com>
2025-09-21 18:44:32 +0300

    Add files via upload

---
+++ Source.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+    double B = 500.0;
+    double x = 2.5;
+    double width = 0.8;
+    double cost;
+
+    cost = B * x * width;
+
+    printf("§³§ä§à§Ú§Þ§à§ã§ä§î §ä§Ü§Ñ§ß§Ú: %.2f §â§å§Ò§Ý§Ö§Û", cost);
+
+    return 0;
+}
